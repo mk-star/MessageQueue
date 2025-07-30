e7056fa48b2341fab816d9b474203b50a5ce5c4b
mk-star <ssunn0812@gmail.com>
2025-07-30 14:12:54 +0900

    initialize

---
+++ src/main/java/org/example/messagequeue/HelloController.java
@@ -0,0 +1,13 @@
+package org.example.messagequeue;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam String q) {
+        return "Hello " + q;
+    }
+}
---
+++ src/test/java/org/example/messagequeue/MessageQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.messagequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
