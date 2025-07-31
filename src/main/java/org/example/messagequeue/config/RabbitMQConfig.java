2369c2cc12d55efa086a0c1d8bebefd5ef368570
mk-star <ssunn0812@gmail.com>
2025-07-31 22:47:19 +0900

    feat: 관심사별 Queue Bean 생성 및 Binding 설정 추가

--- src/main/java/org/example/messagequeue/config/RabbitMQConfig.java
+++ src/main/java/org/example/messagequeue/config/RabbitMQConfig.java
@@ -6,23 +6,44 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    public static final String QUEUE_NAME = "notificationQueue";
-    public static final String FANOUT_EXCHANGE = "notificationExchange";
+    public static final String FANOUT_EXCHANGE_FOR_NEWS = "newsExchange";
+
+    public static final String JAVA_QUEUE = "javaQueue";
+    public static final String SPRING_QUEUE = "springQueue";
+    public static final String VUE_QUEUE = "vueQueue";
 
     @Bean
-    public Queue notificationQueue() {
-        return new Queue(QUEUE_NAME, false);
+    public Queue javaQueue() {
+        return new Queue(JAVA_QUEUE, false);
+    }
+
+    @Bean
+    public Queue springQueue() {
+        return new Queue(SPRING_QUEUE, false);
+    }
+
+    @Bean
+    public Queue vueQueue() {
+        return new Queue(VUE_QUEUE, false);
     }
 
     @Bean
     public FanoutExchange fanoutExchange() {
-        // 메시지를 수신하면 연결된 모든 큐로 브로드캐스트
-        return new FanoutExchange(FANOUT_EXCHANGE);
+        return new FanoutExchange(FANOUT_EXCHANGE_FOR_NEWS);
     }
 
     @Bean
-    public Binding bindNotification(Queue notificationQueue, FanoutExchange fanoutExchange) {
-        // BindingBuilder.bind.to() 를 통해 Queue와 Exchange를 연결
-        return BindingBuilder.bind(notificationQueue).to(fanoutExchange);
+    public Binding javaBinding(Queue javaQueue, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(javaQueue).to(fanoutExchange);
+    }
+
+    @Bean
+    public Binding springBinding(Queue springQueue, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(springQueue).to(fanoutExchange);
+    }
+
+    @Bean
+    public Binding vueBinding(Queue vueQueue, FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(vueQueue).to(fanoutExchange);
     }
 }
