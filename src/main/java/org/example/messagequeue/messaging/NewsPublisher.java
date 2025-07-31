a48e363aa6614defaa921f2371c12410b82befee
mk-star <ssunn0812@gmail.com>
2025-07-31 22:49:30 +0900

    feat: 뉴스 기반 Pub/Sub 모델 클래스 구현

---
+++ src/main/java/org/example/messagequeue/messaging/NewsPublisher.java
@@ -0,0 +1,30 @@
+package org.example.messagequeue.messaging;
+
+import org.example.messagequeue.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewsPublisher {
+    private final RabbitTemplate rabbitTemplate;
+
+    public NewsPublisher(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    private String publishMessage(String news, String messageSuffix) {
+        String message = news + messageSuffix;
+
+        rabbitTemplate.convertAndSend(RabbitMQConfig.FANOUT_EXCHANGE_FOR_NEWS, news, message);
+        System.out.println("News Published: " + message);
+        return message;
+    }
+
+    public String publish(String news) {
+        return publishMessage(news, " 관련 새 소식이 있어요!");
+    }
+
+    public String publishAPI(String news) {
+        return publishMessage(news, " - 관련 새 소식이 나왔습니다. (API)");
+    }
+}
