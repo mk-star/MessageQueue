aeaeb628f539b86e6be8981dee0a73b0764c43d2
mk-star <ssunn0812@gmail.com>
2025-08-01 14:40:24 +0900

    feat: 로그 발행/소비 객체 생성

---
+++ src/main/java/org/example/messagequeue/messaging/LogConsumer.java
@@ -0,0 +1,23 @@
+package org.example.messagequeue.messaging;
+
+import org.example.messagequeue.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogConsumer {
+    @RabbitListener(queues = RabbitMQConfig.ERROR_QUEUE)
+    public void consumeError(String message) {
+        System.out.println("[ERROR]를 받음 : " + message);
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.WARN_QUEUE)
+    public void consumeWarn(String message) {
+        System.out.println("[WARN]를 받음 : " + message);
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.INFO_QUEUE)
+    public void consumeInfo(String message) {
+        System.out.println("[INFO]를 받음 : " + message);
+    }
+}
