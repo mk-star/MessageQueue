a1854fe573961d937c0accac5904c4c313567805
mk-star <ssunn0812@gmail.com>
2025-08-02 15:44:16 +0900

    feat: Retry 및 DLQ 처리를 application.yml 설정으로 위임

--- src/main/java/org/example/messagequeue/messaging/OrderConsumer.java
+++ src/main/java/org/example/messagequeue/messaging/OrderConsumer.java
@@ -2,39 +2,19 @@ package org.example.messagequeue.messaging;
 
 import org.example.messagequeue.config.RabbitMQConfig;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderConsumer {
-    private final RabbitTemplate rabbitTemplate;
-    private final RetryTemplate retryTemplate;
-
-    public OrderConsumer(RabbitTemplate rabbitTemplate, RetryTemplate retryTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-        this.retryTemplate = retryTemplate;
-    }
+    private int retryCount;
 
     @RabbitListener(queues = RabbitMQConfig.ORDER_COMPLETED_QUEUE)
-    public void consume(String message) {
-        retryTemplate.execute(context -> {
-            try {
-                System.out.println("# 리시브 메시지 : " + message + " # retry : " + context.getRetryCount());
-                // 실패 조건
-                if ("fail".equalsIgnoreCase(message)) {
-                    throw new RuntimeException(message);
-                }
-                System.out.println("# 메시지 처리 성공 " + message);
-            } catch (Exception e) {
-                if (context.getRetryCount() >= 2) {
-                    rabbitTemplate.convertAndSend(RabbitMQConfig.ORDER_TOPIC_DLX,
-                            RabbitMQConfig.DEAD_LETTER_ROUTING_KEY, message);
-                } else {
-                    throw e;
-                }
-            }
-            return null;
-        });
+    public void processMessage(String message) {
+        System.out.println("Received message: "+ message + "count : " + retryCount++);
+        if ("fail".equals(message)) {
+            throw new RuntimeException("- Processing failed. Retry" );
+        }
+        System.out.println("Message processed successfully: " + message);
+        retryCount = 0;
     }
 }
