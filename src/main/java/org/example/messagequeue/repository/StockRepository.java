bd6f971b22f693d07ca022867ec1678a6c644bc5
mk-star <ssunn0812@gmail.com>
2025-08-05 18:52:00 +0900

    remove: 사용하지 않는 파일 제거

--- src/main/java/org/example/messagequeue/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.messagequeue.repository;
-
-import org.example.messagequeue.entity.StockEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<StockEntity, Long> {
-}
